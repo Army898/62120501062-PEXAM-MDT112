d5d23513276ce5cf75dfe6e2bddb1a31eb9020f5
Army898 <61343355+Army898@users.noreply.github.com>
2020-07-04 15:13:34 +0700

    7. Complete

--- 062-PEXAM/062-PEXAM.ino
+++ 062-PEXAM/062-PEXAM.ino
@@ -7,14 +7,17 @@
 Adafruit_SSD1306 oled = Adafruit_SSD1306(128, 32, &Wire);
 Servo myservo;        
 int pos = 0;
-
+bool lastState = true;
 void setup()
 {
+    pinMode(2, INPUT_PULLUP);
     oled.begin(0x3C);
     tone(8,300,500);   
     Serial.begin(9600);
     Serial.println("Hello MDT");
-    myservo.attach(9);  
+    myservo.attach(9);
+    myservo.write(pos);  
+    delay(200);
     for(pos = 0; pos < 180; pos +=1) 
     {
         myservo.write(pos); 
@@ -38,6 +41,15 @@ void setup()
     delay(650);
     tone(8,600,400);
 }
-void loop(){
+int count = 0;
+void loop()
+{
+    bool currentState = digitalRead(2);
 
+    if(lastState > currentState)                
+    {
+        count++;
+        myservo.write(pos + (count*10));                                 
+    }
+    lastState = currentState;
 }
