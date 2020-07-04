7a21793fa6f866e18a3a3cc0098aca931ad33948
Army898 <61343355+Army898@users.noreply.github.com>
2020-07-04 14:15:04 +0700

    Edit 3. Complete

--- 062-PEXAM/062-PEXAM.ino
+++ 062-PEXAM/062-PEXAM.ino
@@ -4,13 +4,14 @@ int pos = 0;
 
 void setup()
 {
-    tone(8,300,500);
+    tone(8,300,500);   
     Serial.begin(9600);
     Serial.println("Hello MDT");
     myservo.attach(9);  
     for(pos = 0; pos < 180; pos +=1) 
     {
         myservo.write(pos); 
+        delay(15);
     }
       Serial.println("Servo : " + String(pos) + " degrees");
       delay(900);
